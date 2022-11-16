c53a7a3e5485b223265f886e5e29300ae567c210
talismanchik <talismanchik.by@gmail.com>
2022-11-16 12:03:08 +0300

    lesson 23

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,7 +5,7 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div className={s.postsBlock}>
-            my posts
+            <h3>My posts</h3>
             <div>
                 <div><textarea></textarea></div>
                 <div>
